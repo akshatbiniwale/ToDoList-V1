c4a11224ce375aafc730084f787f3bb86b0d03e9
Akshat Biniwale <akshatbiniwale@gmail.com>
2023-05-27 15:12:37 +0530

    List Navigation Pending

--- app.js
+++ app.js
@@ -1,76 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const taskManager = require("./mongodb");
+const _ = require('lodash');
 
 const app = express();
-var errMsg = "";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const today = new Date();
 var listHead = "home"
-var day
+const options = { 
+    weekday: 'long',
+    month: 'long', 
+    day: 'numeric' 
+};
+
+const day = today.toLocaleDateString("en-US", options);
 
 app.get("/home", async function(req, res){
-    var today = new Date();
-    var currentDay = today.getDay();
-    day="";
-
-    var options = { 
-        weekday: 'long',
-        month: 'long', 
-        day: 'numeric' 
-    };
-
-    day = today.toLocaleDateString("en-US", options);
-
     const taskList = await taskManager.firstSchema.find();
 
-    res.render("list", {listTitle: day, listHead: listHead, listItem: taskList, errorMessage: errMsg});
+    res.render("list", {listTitle: day, listHead: _.capitalize(listHead), listItem: taskList});
 });
 
 app.post("/home", async function(req, res){
 
     const taskVal = req.body.task;
-    if(taskVal != ''){
-        errMsg = ""
-        const data = {
-            name: taskVal
-        }
-        await taskManager.firstSchema.insertMany([data]);
-    }else{
-        errMsg = "Error: Value is NULL"
+    const data = {
+        name: taskVal
     }
+    await taskManager.firstSchema.insertMany([data]);
     res.redirect("/home");
 });
 
-app.post("/delete", function(req, res){
+app.post("/:category/delete", function(req, res){
     setTimeout(async () => {
-        await taskManager.firstSchema.deleteOne({_id: req.body.checks})
-        res.redirect("/home");
+        const categoryList = req.params.category;
+        if(categoryList == "home"){
+            await taskManager.firstSchema.deleteOne({_id: req.body.checks})
+        }else{
+            await taskManager.secondSchema.deleteOne({_id: req.body.checks})
+        }
+        res.redirect("/" + categoryList);
     }, 500);
 });
 
 app.get("/:category", async function(req, res){
     const categoryList = req.params.category;
     const taskList = await taskManager.secondSchema.find({category: categoryList});
-    res.render("list", {listTitle: day, listHead: categoryList, listItem: taskList, errorMessage: errMsg});
+    res.render("list", {listTitle: day, listHead: _.capitalize(categoryList), listItem: taskList});
 });
 
 app.post("/:category", async function(req, res){
     const categoryList = req.params.category;
     const taskVal = req.body.task
-    console.log(taskVal + " + " + categoryList)
-    if(taskVal != ''){
-        const data = {
-            category: categoryList,
-            name: taskVal
-        }
-        await taskManager.secondSchema.insertMany([data]);
-    }else{
-        errMsg = "Error: Value is NULL"
+    const data = {
+        category: categoryList,
+        name: taskVal
     }
+    await taskManager.secondSchema.insertMany([data]);
     res.redirect("/" + categoryList);
 })
 
